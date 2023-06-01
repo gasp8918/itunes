e8d2face6cdc29eefc3ab660c1e81b825af3cc45
Rocco Gasperini <gasp8918@student.acitech.org>
2023-06-01 09:18:01 -0400

    merge all the functions we need into one to avoid dumb

--- script.js
+++ script.js
@@ -5,30 +5,25 @@ const ITEM_TEMPLATE = '<a class="result" href="!"><img src="!"<br>!</a>' // plan
 var current_url
 var response
 var searchType = "artistTerm" // later we'll add a dropdown and let peoplepick from songs and artists and albums to search
-SUBMIT.addEventListener("click", ()=> {parseList(getList(getURL(INPUT.value)))})
-
-
-
-function getList(url) {
-    fetch(url)
+SUBMIT.addEventListener("click", ()=> {
+    let search = INPUT.Value
+    search = search.replace(/\s+/g, '+');; // might get rid of the spaces in a string
+    current_url = API_URL.concat(search, "&media=music&attribute=", searchType);
+    
+    fetch(current_url)
     .then((res) => res.json()) // stole from the dad joke one lol
         .then((data)=> {
             console.log(data)
         })
-}
+    
+    for (i=0;i<data.resultCount; i++) { // do this later lol
+
+    }
+})
+
 
 function getArt(url) {
     url = url.slice(0, (url.length - 13)) // here we will get a higher quality picture of the album art by manually changing the quality from the json's link.
     url  = url.concat(url, "500x500bb.jpg")
     return url
-}
-
-function getURL(search) { // generates the url link we'll ask the itunes api about
-    search = search.replace(/\s+/g, '+');; // might get rid of the spaces in a string
-    current_url = API_URL.concat(search, "&media=music&attribute=", searchType);  
-    return current_url 
-}
-
-function parseList(data) {
-    
 }
