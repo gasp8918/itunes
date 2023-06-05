c5a989192104919b628cb374cb04e727114b8cb1
Rocco Gasperini <gasp8918@student.acitech.org>
2023-06-05 09:17:21 -0400

    style things, and search size

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const SUBMIT = document.getElementById("submit")
 const INPUT = document.getElementById("input")
 const LIST = document.getElementById("list")
 const API_URL = "https://itunes.apple.com/search?term="
+const NUM = document.getElementById("num")
 const ITEM_TEMPLATE = '<a class="result" href="!"><img src="!"><br>!</a>' // plan is to replace ! in a loop, as replace() only replaces the first instance of a letter, and then shove it into the list
+
 var current_url
 var response
 var searchType = "artistTerm" // later we'll add a dropdown and let peoplepick from songs and artists and albums to search
@@ -13,7 +15,7 @@ SUBMIT.addEventListener("click", ()=> {
     let search = INPUT.value
     var result
     search = search.replace(/\s+/g, '+'); // might get rid of the spaces in a string
-    current_url = API_URL.concat(search, "&media=music&attribute=", searchType);
+    current_url = API_URL.concat(search, "&media=music&attribute=", searchType, "&limit=",NUM.value);
     
     fetch(current_url)
     .then((res) => res.json()) // stole from the dad joke one lol
@@ -26,7 +28,7 @@ SUBMIT.addEventListener("click", ()=> {
                 let art = getArt(result.results[i].artworkUrl100)
                 element = element.replace("!", result.results[i].trackViewUrl)
                 element = element.replace("!", art)
-                element = element.replace("!", result.results[i].trackName)
+                element = element.replace("!", checkName(result.results[i].trackName))
                 let elm = document.createElement("a")
                 LIST.appendChild(elm)
                 console.log(element)
@@ -35,6 +37,13 @@ SUBMIT.addEventListener("click", ()=> {
         })
 })
 
+function checkName(name) {
+    if (name.length > 27) {
+        name = name.slice(0,27)
+        name = name.concat("...")
+    }
+    return name
+}
 
 function getArt(url) {
     url = url.slice(0, (url.length - 13)) // here we will get a higher quality picture of the album art by manually changing the quality from the json's link.
