c2c1e05d757ff73f641527adf7ae83889aff1f9c
Rocco Gasperini <gasp8918@student.acitech.org>
2023-06-05 08:32:47 -0400

    make it function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const SUBMIT = document.getElementById("submit")
 const INPUT = document.getElementById("input")
+const LIST = document.getElementById("list")
 const API_URL = "https://itunes.apple.com/search?term="
-const ITEM_TEMPLATE = '<a class="result" href="!"><img src="!"<br>!</a>' // plan is to replace ! in a loop, as replace() only replaces the first instance of a letter, and then shove it into the list
+const ITEM_TEMPLATE = '<a class="result" href="!"><img src="!"><br>!</a>' // plan is to replace ! in a loop, as replace() only replaces the first instance of a letter, and then shove it into the list
 var current_url
 var response
 var searchType = "artistTerm" // later we'll add a dropdown and let peoplepick from songs and artists and albums to search
 SUBMIT.addEventListener("click", ()=> {
+    while (LIST.hasChildNodes()) {
+        LIST.removeChild(LIST.firstChild);
+      }
     let search = INPUT.value
     var result
     search = search.replace(/\s+/g, '+'); // might get rid of the spaces in a string
@@ -24,6 +28,7 @@ SUBMIT.addEventListener("click", ()=> {
                 element = element.replace("!", art)
                 element = element.replace("!", result.results[i].trackName)
                 let elm = document.createElement("a")
+                LIST.appendChild(elm)
                 console.log(element)
                 elm.innerHTML = element // fix please plea seplase plase
             }
@@ -33,6 +38,6 @@ SUBMIT.addEventListener("click", ()=> {
 
 function getArt(url) {
     url = url.slice(0, (url.length - 13)) // here we will get a higher quality picture of the album art by manually changing the quality from the json's link.
-    url  = url.concat(url, "500x500bb.jpg")
+    url  = url.concat("500x500bb.jpg")
     return url
 }
